d33ccca88696d31ae8da28da8130f2f2d6db7c67
Your Name <red-laurie@hotmail.co.uk>
2021-06-12 14:34:46 +0100

    Feat: Update and Delete
    Users can now Update Contacts aswell as Delete contacts. It uses two built in views with Django being UpdateView and DeleteView.

--- vipra_technical/contacts/urls.py
+++ vipra_technical/contacts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-from .views import ContactCreateView
+from .views import ContactCreateView, ContactUpdateView, ContactDeleteView
 
 
 urlpatterns = [
     path('',views.home, name = 'contacts-home'),
     path('contact/<int:pk>/', views.DetailView, name='contact-details'),
     path('contact/new/', ContactCreateView.as_view(), name='contact-create'),
+    path('contact/<int:pk>/update/', ContactUpdateView.as_view(), name='contact-update'),
+    path('contact/<int:pk>/delete/', ContactDeleteView.as_view(), name='contact-delete'),
 ]
--- vipra_technical/contacts/views.py
+++ vipra_technical/contacts/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from .models import Contact
 from django.shortcuts import render,get_object_or_404
-from django.views.generic import CreateView,UpdateView
+from django.views.generic import CreateView,UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 # Create your views here.
 
 def home(request):
@@ -22,3 +23,28 @@ class ContactCreateView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class ContactUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Contact
+    fields = ['name','email','Number']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        contact = self.get_object()
+        if self.request.user == contact.author:
+            return True
+        else:
+            return False
+
+class ContactDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Contact
+    success_url = '/'
+
+    def test_func(self):
+        contact = self.get_object()
+        if self.request.user == contact.author:
+            return True
+        return False
