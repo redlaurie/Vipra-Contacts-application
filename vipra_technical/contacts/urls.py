55d86ddda2928ac3e56253f6aa61611ec4e1e82a
Your Name <red-laurie@hotmail.co.uk>
2021-06-19 11:56:17 +0100

    Feat: Class based API view
    I followed the documentation further to learn how to do Class based API views as suggested in the interview.

--- vipra_technical/contacts/urls.py
+++ vipra_technical/contacts/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('contacts-list/', views.ContacsList, name='contacts-list'),
     path('contacts-create/', views.ContacsCreate, name='contacts-create'),
     path('contacts-update/<str:pk>/', views.ContacsUpdate, name='contacts-update'),
     path('contacts-delete/<str:pk>/', views.ContacsDelete, name='contacts-delete'),
+    path('API/', views.ContactList.as_view()),
+    path('API/<str:pk>/', views.ContactActions.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- vipra_technical/contacts/views.py
+++ vipra_technical/contacts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import Http404
 from .models import Contact
+from rest_framework import status
 from django.shortcuts import render,redirect,get_object_or_404
 from django.views.generic import CreateView,UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
@@ -16,8 +17,38 @@ from rest_framework.response import Response
 from .serializers import ContactSerializer
 
 class ContactList(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    def get(self, request, format=None):
+        contacts = Contact.objects.all()
+        serializer = ContactSerializer(contacts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ContactActions(APIView):
     print("hello")
 
+    def put(self,request,pk,format=None):
+        contact = Contact.objects.get(id = pk)
+        serializer = ContactSerializer(contact,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        contact = Contact.objects.get(id = pk)
+        contact.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET'])
 def home(request):
